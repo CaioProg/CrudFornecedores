5cbf619233548e724a628a2abfe9decc9568fe14
Caio Lucas <caio.lucas@ploomes.com>
2023-04-08 00:16:30 -0300

    Create interaction record in ploomes

--- CrudFornecedores/Integrations/Ploomes.cs
+++ CrudFornecedores/Integrations/Ploomes.cs
@@ -39,9 +39,10 @@ namespace CrudFornecedores.Integrations
 					var id = res.value[0].Id;
 
 					fornecedor.IdPloomes = id;
-				}
 
-				//Console.WriteLine(response.StatusCode);
+					await CreateInterationRecordPloomesAsync(fornecedor);
+
+				}
 			}
 			catch (Exception ex)
 			{
@@ -81,6 +82,36 @@ namespace CrudFornecedores.Integrations
 				Console.WriteLine($"{ex.Message}");
 			}
 			
+		}
+		internal static async Task CreateInterationRecordPloomesAsync(Fornecedor fornecedor)
+		{
+			var url = "https://app24-api2.ploomes.com/InteractionRecords";
+			string key = "7DC64F4804E229652F62193E29E18776A5C9007DCC3E1B7A0392A71F19CFE9C67D761A4580DC9A88300905DA065401EAADB01CF988E80E408C0C80ED559C98F4";
+
+
+			try
+			{
+				IDictionary<string, string> body = new Dictionary<string, string>()
+				{
+					{"ContactId", $"{fornecedor.IdPloomes}"},
+					{"Content", $"Fornecedor {fornecedor.Nome} criado com sucesso pela integração!"}
+				};
+
+				var client = new HttpClient();
+				var request = new HttpRequestMessage();
+				var content = new StringContent(System.Text.Json.JsonSerializer.Serialize(body), Encoding.UTF8, "application/json");
+				content.Headers.Add($"User-Key", key);
+
+				var response = await client.PostAsync(url, content);
+
+				var resposta = await response.Content.ReadAsStringAsync();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"{ex.Message}");
+			}
+
+
 		}
 	}
 }
